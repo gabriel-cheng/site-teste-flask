4d2d7d20ec76d238a3ee9171920e90f7e0e73d8c
Gabriel <gabriel_carvalho.contato@outlook.com>
2021-12-17 23:32:27 -0300

    Chamando todos os templates ddo site

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
-from flask  import Flask
+from flask  import Flask # Importa o framework Flask
+from flask import render_template # Importa a função de invocar templates
 
 
 app = Flask(__name__) # O flask recomenda que você use o nome "app" como nome
@@ -8,11 +9,21 @@ app = Flask(__name__) # O flask recomenda que você use o nome "app" como nome
 # Quando você começa com "@", se torna um decorator. Basicamente, abaixo está falando que a função "homepage" vai ser exibita na home (página inicial)
 @app.route("/") # É a rota do site, basicamente o link do site.
 def homepage():
-    return "Este é meu primeiro site usando Python/Flask" # Retorna o que eu quero que exiba na página
+    return render_template("homepage.html") # Template da homepage
 
 
 @app.route("/contato")
 def contatos():
-     return "<p>Nosso contato é</p><p>E-mail: gabriel_carvalho.contato@outlook.com</p><p>Celular: (18)99116-3599</p>"
-# Subir o site
-app.run(debug = True) # Carrega o site
+     return render_template("contato.html") # Template de contato
+
+
+@app.route("/usuarios/<nome_user>") # Basicamente, o que for inserido dentro "< >" será o nome do usuário
+def usuarios(nome_user): # Ela recebe o nome do usuário como parâmetro em "nome_user"
+    return render_template("usuarios.html", variavel_nome_html = nome_user)
+    # "variavel_nome_html" é o nome da variável que foi importada no corpo do HTML
+    # Segundo "nome_user" é o parâmetro utilizado na função
+
+
+if __name__ == "__main__":
+    # Subir o site
+    app.run(debug = True) # Carrega o site
